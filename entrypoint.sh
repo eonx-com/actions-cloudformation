4e2d2f24079501033cb97a3a3b98695f25048853
Damian Sloane <dddaaammmooo@hotmail.com>
2019-11-14 18:12:29 +1100

    Interim commit, work in progress...

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,3 +16,6 @@ cd /tmp/actions-cloudformation;
 ls -l
 source ./venv/bin/activate;
 python ./build.py "${ENVIRONMENT_NAME}" "${ENVIRONMENT_PATH}" "${TEMPLATE_PATH}" "${GITHUB_WORKSPACE}/${OUTPUT_FILENAME}"
+
+ls -l ${GITHUB_WORKSPACE};
+cat "${GITHUB_WORKSPACE}/${OUTPUT_FILENAME}"
