176263241d30bcdab46141bb496d665b81403ecb
Damian Sloane <dddaaammmooo@hotmail.com>
2019-11-15 08:26:55 +1100

    Interim commit, work in progress...

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ echo "Output Filename:  ${OUTPUT_FILENAME}"
 cd /tmp/actions-cloudformation;
 ls -l
 source ./venv/bin/activate;
-python ./build.py "${ENVIRONMENT_NAME}" "${ENVIRONMENT_PATH}" "${TEMPLATE_PATH}" "${GITHUB_WORKSPACE}/${OUTPUT_FILENAME}"
+python ./build.py "${ENVIRONMENT_NAME}" "${GITHUB_WORKSPACE}/${ENVIRONMENT_PATH}" "${GITHUB_WORKSPACE}/${TEMPLATE_PATH}" "${GITHUB_WORKSPACE}/${OUTPUT_FILENAME}"
 
 ls -l ${GITHUB_WORKSPACE};
 cat "${GITHUB_WORKSPACE}/${OUTPUT_FILENAME}"
